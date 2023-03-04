90c23294e44eac25033fe0e395e0cd509079cc98
moringspeaker <guchenyu150533@gmail.com>
2023-03-04 03:04:07 -0500

    v10

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,7 @@
-from time import timezone
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
-from django.template import loader
 from django.urls import reverse
 from django.views import generic
 
